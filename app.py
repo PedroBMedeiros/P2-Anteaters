6ee45f6636b216b35458fbdb3eceffc3460e9bf9
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2021-03-27 15:27:58 -0700

    fix

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ db = SQLAlchemy(app)
 def home():
     return render_template('index.html')
 
+
 if __name__ == "__main__":
     # runs the application on the repl development server
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port="5000")
+#192.168.86.51
