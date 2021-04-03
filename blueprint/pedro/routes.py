e7a7d8ad943381ed0c25fa9b0720b456188d5367
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2021-04-02 23:49:59 -0700

    Individual mini lab... classes and objects

--- blueprint/pedro/routes.py
+++ blueprint/pedro/routes.py
@@ -2,6 +2,14 @@ from flask import render_template, Blueprint
 
 pedro = Blueprint('pedro', __name__)
 
+
+class Intro:
+    name = "Pedro"
+
+
+intro = Intro()
+
+
 @pedro.route("/pedro")
 def pedroRoute():
-    return render_template("Introduction.html", name="pedro")
+    return render_template("Introduction.html", name=intro.name)
