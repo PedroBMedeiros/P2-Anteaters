c7abe712a07c76fefabc681ff94d115b96427bcb
Giustanthony <72960297+Giustanthony@users.noreply.github.com>
2021-04-07 14:07:03 -0700

    finishing touches to mini lab

--- blueprint/anthony/routes.py
+++ blueprint/anthony/routes.py
@@ -4,6 +4,8 @@ anthony = Blueprint('anthony', __name__)
 
 @anthony.route("/anthony", methods=['GET','POST'])
 def anthonyRoute():
-    a = int(request.form.get("a", 3))
-    cube_output = a**3
-    return render_template("anthony.html", cube_output=cube_output, a=a)
+    class alg:
+        a = int(request.form.get("a", 3))
+        cube_output = a**3
+    res = alg()
+    return render_template("anthony.html", cube_output=alg.cube_output, a=alg.a)
