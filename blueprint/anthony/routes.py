a591cc221a6bdf0467b7670dac4d6d3a3d28fb5d
Giustanthony <72960297+Giustanthony@users.noreply.github.com>
2021-04-07 10:12:40 -0700

    mini lab

--- blueprint/anthony/routes.py
+++ blueprint/anthony/routes.py
@@ -4,11 +4,6 @@ anthony = Blueprint('anthony', __name__)
 
 @anthony.route("/anthony", methods=['GET','POST'])
 def anthonyRoute():
-    a = int(request.form.get("a", 1))
-    b = int(request.form.get("b", 1))
-    c = int(request.form.get("c", 1))
-    d = (b**2) - (4*a*c)
-    sol1 = (-b-cmath.sqrt(d))/(2*a)
-    sol2 = (-b+cmath.sqrt(d))/(2*a)
-    output = 'The solution are {0} and {1}'.format(sol1,sol2)
-    return render_template("Introduction.html", name="Anthony", output=output, a=a, b=b, c=c)
+    a = int(request.form.get("a", 3))
+    cube_output = a**3
+    return render_template("anthony.html", cube_output=cube_output, a=a)
