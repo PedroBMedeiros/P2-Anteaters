ba2477f18836ce07e2dd9305c0f0c20de9fe64c8
kenzie-rylie <kenzie.rylie16@gmail.com>
2021-04-06 19:14:47 -0700

    fixed mini lab

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect, CSRFError
 from blueprint.kenzie.routes import kenzie
 from blueprint.anthony.routes import anthony
 from blueprint.naweid.routes import naweid
 from blueprint.pedro.routes import pedro
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "MY SECRET KEY!"
 app.register_blueprint(anthony)
 app.register_blueprint(kenzie)
 app.register_blueprint(naweid)
 app.register_blueprint(pedro)
-
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 db = SQLAlchemy(app)
 
 @app.route('/')
