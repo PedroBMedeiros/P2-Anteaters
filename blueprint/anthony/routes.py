8657fae11ace0a28f113e9a6a879476cd76ba15e
kenzie-rylie <kenzie.rylie16@gmail.com>
2021-04-06 22:43:05 -0700

    fixed mini lab

--- blueprint/anthony/routes.py
+++ blueprint/anthony/routes.py
@@ -1,11 +1,16 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+import cmath
 anthony = Blueprint('anthony', __name__)
 
-class Intro:
-    name = "Anthony"
-    
-intro = Intro()
-
-@anthony.route("/anthony")
+@anthony.route("/anthony", methods=['GET','POST'])
 def anthonyRoute():
-    return render_template("Introduction.html", name=intro.name)
+    a = int(request.form.get("a", 1))
+    b = int(request.form.get("b", 1))
+    c = int(request.form.get("c", 1))
+    # calculate the discriminant
+    d = (b**2) - (4*a*c)
+    # find two solutions
+    sol1 = (-b-cmath.sqrt(d))/(2*a)
+    sol2 = (-b+cmath.sqrt(d))/(2*a)
+    output = 'The solution are {0} and {1}'.format(sol1,sol2)
+    return render_template("Introduction.html", name="Anthony", output=output, a=a, b=b, c=c)
