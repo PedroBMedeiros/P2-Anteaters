607d32d96a96b0cc887900be9b27c21181cf9254
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2021-04-07 19:08:36 -0700

    adding minilab

--- blueprint/pedro/routes.py
+++ blueprint/pedro/routes.py
@@ -1,8 +1,16 @@
 from flask import render_template, Blueprint, request
+import math
 pedro = Blueprint('Pedro', __name__)
 
 
 @pedro.route("/pedro", methods=['GET','POST'])
 def pedroRoute():
+    class pythag:
+        a = int(request.form.get("a", 1))
+        b = int(request.form.get("b", 1))
+        c = math.sqrt(a**2 + b**2)
+        output = c
+    class_info = pythag()
+    return render_template("PedroIntroduction.html", output=class_info.output, a=class_info.a, b=class_info.b, name="Pedro")
 
-    return render_template("Introduction.html", name="pedro")
+    # return render_template("Introduction.html", name="pedro")
