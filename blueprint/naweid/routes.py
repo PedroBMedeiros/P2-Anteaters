f227cb73f93ad23a23bd20d24982cfeab0b0037e
Naweid <72881854+Naweid@users.noreply.github.com>
2021-04-07 22:04:16 -0700

    Update routes.py

--- blueprint/naweid/routes.py
+++ blueprint/naweid/routes.py
@@ -1,6 +1,13 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 naweid = Blueprint('naweid', __name__)
 
 @naweid.route("/naweid", methods=['GET','POST'])
 def naweidRoute():
-    return render_template("naweid.html", name="Naweid")
+    class add:
+        a = int(request.form.get("a", 1))
+        b = int(request.form.get("b", 1))
+        add_output = (b+a)
+
+    nawo = add()
+
+    return render_template("naweid.html", add_output=nawo.add_output, a=add.a, b=add.b, name="Naweid")
