523c309f7b9f4be5c9993fcea1272b8dd18308f8
Giustanthony <72960297+Giustanthony@users.noreply.github.com>
2021-04-20 17:15:42 -0700

    thing

--- blueprint/anthony/routes.py
+++ blueprint/anthony/routes.py
@@ -3,8 +3,20 @@ anthony = Blueprint('anthony', __name__)
 
 @anthony.route("/anthony", methods=['GET','POST'])
 def anthonyRoute():
-    class alg:
-        a = int(request.form.get("a", 3))
-        cube_output = a**3
-    res = alg()
-    return render_template("anthony.html", cube_output=alg.cube_output, a=alg.a, name="Anthony")
+    class ALG:
+        def __init__(self, a = 0):
+            self._a = a
+
+        # getter method
+        def get_a(self):
+            return self._a
+
+        # setter method
+        def set_a(self, x):
+            self._a = x
+
+    alg = ALG()
+    alg.set_a(int(request.form.get("a", 3)))
+    cube_output = alg.get_a()**3
+    a = alg.get_a()
+    return render_template("anthony.html", cube_output=cube_output, a=a, name="Anthony")
