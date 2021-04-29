62c2871480f2ea84df7d5b180e70dbea52ff1deb
Giustanthony <72960297+Giustanthony@users.noreply.github.com>
2021-04-29 16:37:16 -0700

    bubble

--- blueprint/anthony/routes.py
+++ blueprint/anthony/routes.py
@@ -17,4 +17,21 @@ def anthonyRoute():
     alg.set_a(int(request.form.get("a", 3)))
     a = alg.get_a()
     cube_output = a**3
-    return render_template("anthony.html", cube_output=cube_output, a=a, name="Anthony")
+
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    original = request.form.getlist('input_text[]')
+    arr = [1, 2]
+    if request.method != 'GET':
+        if original[0].isnumeric():
+            arr = [int(i) for i in original]
+        else:
+            arr = original
+        bubbleSort(arr)
+
+    return render_template("anthony.html", cube_output=cube_output, a=a, name="Anthony", original=original, sorted=arr)
