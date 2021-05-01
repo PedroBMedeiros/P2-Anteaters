c0d61c1ade4341f8d8ecc2e9cfa46fbebbd0e0a0
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2021-04-30 18:13:21 -0700

    pedro bubble

--- blueprint/pedro/routes.py
+++ blueprint/pedro/routes.py
@@ -9,8 +9,23 @@ def pedroRoute():
         a = int(request.form.get("a", 1))
         b = int(request.form.get("b", 1))
         c = math.sqrt(a**2 + b**2)
-        output = c
+        pythag_output = c
     class_info = pythag()
-    return render_template("pedro.html", output=class_info.output, a=class_info.a, b=class_info.b, name="Pedro")
 
-    # return render_template("Introduction.html", name="pedro")
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    original = request.form.getlist('input_text[]')
+    arr = [1, 2]
+    if request.method != 'GET':
+        if original[0].isnumeric():
+            arr = [int(i) for i in original]
+        else:
+            arr = original
+        bubbleSort(arr)
+
+    return render_template("pedro.html", output=class_info.pythag_output, a=class_info.a, b=class_info.b, name="Pedro", original=original, sorted=arr)
