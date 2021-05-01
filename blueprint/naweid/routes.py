7d41ef5ccf054d0dab4a7fe2e0fc1fbd56c73496
Naweid <72881854+Naweid@users.noreply.github.com>
2021-04-30 21:15:10 -0700

    Update routes.py

--- blueprint/naweid/routes.py
+++ blueprint/naweid/routes.py
@@ -10,4 +10,20 @@ def naweidRoute():
 
     nawo = add()
 
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1] :
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    original = request.form.getlist('input_text[]')
+    arr = [1, 2]
+    if request.method != 'GET':
+        if original[0].isnumeric():
+            arr = [int(i) for i in original]
+        else:
+            arr = original
+        bubbleSort(arr)
+        
     return render_template("naweid.html", add_output=nawo.add_output, a=add.a, b=add.b, name="Naweid")
