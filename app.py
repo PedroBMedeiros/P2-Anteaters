08764700af7e9e3287c9944041a8b832ab7f7c20
PedroBMedeiros <pedrinho.medeiros.2003@gmail.com>
2021-05-20 17:46:43 -0700

    new contact page with initial messaging system

--- app.py
+++ app.py
@@ -62,6 +62,10 @@ class RegisterForm(FlaskForm):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
