7bff4a6ab53e638f6790400feb5a732426146e3f
Giustanthony <72960297+Giustanthony@users.noreply.github.com>
2021-05-20 22:16:02 -0700

    updated

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from datetime import datetime
-from blueprint.kenzie.routes import kenzie
-from blueprint.anthony.routes import anthony
-from blueprint.naweid.routes import naweid
-from blueprint.pedro.routes import pedro
-from blueprint.cherry.routes import cherry
 
 app = Flask(__name__)
-app.register_blueprint(anthony)
-app.register_blueprint(kenzie)
-app.register_blueprint(naweid)
-app.register_blueprint(pedro)
-app.register_blueprint(cherry)
 app.config['SECRET_KEY'] = 'I<+g/P2N$}0GXOf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///relationships.db'
 db = SQLAlchemy(app)
+CORS(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-csrf = CSRFProtect(app)
-csrf.init_app(app)
 login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,58 +29,80 @@ class User(UserMixin, db.Model):
 
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    condition = db.Column(db.String(100), nullable=False)
-    temp = db.Column(db.Integer)
+    name = db.Column(db.String(120), nullable=False)
     desc = db.Column(db.Text, nullable=False)
+    completed = db.Column(db.Boolean)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+@app.route('/add_todo/<username>', methods=['POST'])
+def add_todo(username):
+    d = request.get_json()
+    user = User.query.filter_by(username=username).first()
+    todo = Todos(name=d['name'], desc=d['desc'], completed=d['completed'], author=user)
+    db.session.add(todo)
+    db.session.commit()
+    return 'Added Todo', 201
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
+@app.route('/remove_todo', methods=['DELETE'])
+def remove_todo():
+    d = request.get_json()
+    todo = Todos.query.get(int(d['id']))
+    db.session.delete(todo)
+    db.session.commit()
+    return f'Removed Todo', 201
 
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+@app.route('/update_todo/<int:id>/<attr>', methods=['PUT'])
+def add_todos(id, attr):
+    todo = Todos.query.get(id)
+    d = request.get_json()
+    if attr == 'name':
+        todo.name = d['name']
+        db.session.commit()
+        return 'modified name', 201
+    if attr == 'desc':
+        todo.desc = d['desc']
+        db.session.commit()
+        return 'modified desc', 201
+    if attr == 'completed':
+        todo.completed = d['completed']
+        db.session.commit()
+        return 'modified completed', 201
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+
+@app.route('/todos/<username>', methods=['GET'])
+def todos(username):
+    for user in User.query.filter_by(username=username):
+        todos = []
+        for todo in user.todos:
+            todos.append({'id':todo.id, 'name': todo.name, 'desc': todo.desc, 'completed': todo.completed, 'date_added': todo.date_added})
+        response = jsonify({'todos': todos})
+        return response
+    return 'Request failed, make sure user exists (case sensitive).', 404
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    d = request.get_json()
+    hashed_password = generate_password_hash(d['password'], method='sha256')
+    try:
+        new_user = User(username=d['username'], email=d['email'], password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return 'successful, user created', 201
+    except:
+        return 'Something went wrong', 401
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    d = request.get_json()
+    user = User.query.filter_by(username=d['username']).first()
+    if user:
+        if check_password_hash(user.password, d['password']):
+            login_user(user, remember=d['remember'])
+            return 'successful', 200
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-
-        return redirect(url_for('login'))
-
-    return render_template('login.html', form=form)
-
-# ADD IT HERE KENZIE
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
+    return 'failed', 406
 
 if __name__ == "__main__":
     app.run(debug=True)
